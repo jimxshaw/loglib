acc11ca9b6a89a93e48d3d58d6a4addcdb5ccb14
Jim Shaw <jimxshaw@gmail.com>
2022-06-26 09:58:57 -0500

    Add newStore function that creates a new store given a file.

--- internal/log/store.go
+++ internal/log/store.go
@@ -25,3 +25,20 @@ type store struct {
 	buf  *bufio.Writer
 	size uint64
 }
+
+func newStore(f *os.File) (*store, error) {
+	// Getting the file's current size is important just
+	// in case the store is being re-created from a file that
+	// has existing data. E.g. after this service re-started.
+	file, err := os.Stat(f.Name())
+	if err != nil {
+		return nil, err
+	}
+	size := uint64(file.Size())
+
+	return &store{
+		File: f,
+		size: size,
+		buf:  bufio.NewWriter(f),
+	}, nil
+}
