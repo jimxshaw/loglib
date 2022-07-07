43122400d4ad26e3683fd79823986a2a7f504ced
Jim Shaw <jimxshaw@gmail.com>
2022-07-07 18:02:19 -0500

    Add Close test and openFile helper function.

--- internal/log/store_test.go
+++ internal/log/store_test.go
@@ -76,3 +76,39 @@ func testReadAt(t *testing.T, s *store) {
 		offset += int64(numOfBytesRead)
 	}
 }
+
+func testStoreClose(t *testing.T) {
+	f, err := ioutil.TempFile("", "store_close_test")
+	require.NoError(t, err)
+	defer os.Remove(f.Name())
+
+	s, err := newStore(f)
+	require.NoError(t, err)
+	_, _, err = s.Append(write)
+	require.NoError(t, err)
+
+	f, beforeSize, err := openFile(f.Name())
+	require.NoError(t, err)
+
+	err = s.Close()
+	require.NoError(t, err)
+
+	_, afterSize, err := openFile(f.Name())
+	require.NoError(t, err)
+	require.True(t, afterSize > beforeSize)
+}
+
+func openFile(name string) (file *os.File, size int64, err error) {
+	// https://stackoverflow.com/questions/18415904/what-does-mode-t-0644-mean
+	f, err := os.OpenFile(name, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	fileInfo, err := f.Stat()
+	if err != nil {
+		return nil, 0, err
+	}
+
+	return f, fileInfo.Size(), nil
+}
