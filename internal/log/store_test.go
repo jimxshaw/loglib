b1aaf339711def4a55701b62b7d7d5b6fa92d502
Jim Shaw <jimxshaw@gmail.com>
2022-07-07 17:19:21 -0500

    Add TestStoreAppendRead function.

--- internal/log/store_test.go
+++ internal/log/store_test.go
@@ -1,6 +1,36 @@
 package log
 
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
 var (
 	write = []byte("hello world")
 	width = uint64(len(write)) + lenWidth
 )
+
+func TestStoreAppendRead(t *testing.T) {
+	f, err := ioutil.TempFile("", "store_append_read_test")
+	require.NoError(t, err)
+	defer os.Remove(f.Name())
+
+	s, err := newStore(f)
+	require.NoError(t, err)
+
+	testAppend(t, s)
+	testRead(t, s)
+	testReadAt(t, s)
+
+	// Create a new store and test reading from it
+	// again to verify that the service will recover
+	// its state after a restart.
+	s, err = newStore(f)
+	require.NoError(t, err)
+
+	testRead(t, s)
+
+}
