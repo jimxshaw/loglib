767c46a6ca395d4ebc98ccae0170b7e993523a30
Jim Shaw <jimxshaw@gmail.com>
2022-07-12 13:44:45 -0500

    Add index struct and its dependencies, add comments.

--- internal/log/index.go
+++ internal/log/index.go
@@ -1,7 +1,24 @@
 package log
 
+import (
+	"os"
+
+	"github.com/tysonmote/gommap"
+)
+
 var (
+	// Widths define the number of bytes
+	// that make up each index entry.
+	// Entry width is used to go directly to
+	// the position of an entry given its offset,
+	// since the position in the file is offset * entryWidth.
 	offsetWidth   uint64 = 4
 	positionWidth uint64 = 8
-	entireWidth          = offsetWidth + positionWidth
+	entryWidth           = offsetWidth + positionWidth
 )
+
+type index struct {
+	file *os.File
+	mmap gommap.MMap
+	size uint64
+}
