49dd2ff5506157cafd276c4bcdf20754e8c4be50
Jim Shaw <jimxshaw@gmail.com>
2022-09-19 15:34:17 -0500

    Add Append method.

--- internal/log/log.go
+++ internal/log/log.go
@@ -7,6 +7,8 @@ import (
 	"strconv"
 	"strings"
 	"sync"
+
+	api "github.com/jimxshaw/loglib/api/v1"
 )
 
 // The log consists of a list of segments and
@@ -47,6 +49,8 @@ func (l *Log) setup() error {
 		return err
 	}
 
+	// Fetch the list of segments on disk, parse and sort the
+	// base offsets in order from oldest to newest.
 	var baseOffsets []uint64
 	for _, file := range files {
 		offStr := strings.TrimSuffix(file.Name(), path.Ext(file.Name()))
@@ -74,4 +78,25 @@ func (l *Log) setup() error {
 	}
 
 	return nil
+}
+
+// Appends a record to the log. Append the record to the
+// active segment. Make a new active segment if the segment
+// is at its max size (per the max size configuration).
+func (l *Log) Append(record *api.Record) (uint64, error) {
+	// RWMutex is used to grant access to reads when there
+	// isn't a write holding the lock.
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	offset, err := l.activeSegment.Append(record)
+	if err != nil {
+		return 0, err
+	}
+
+	if l.activeSegment.IsMaxed() {
+		err = l.newSegment(offset + 1)
+	}
+
+	return offset, err
 }
